56315908ebe40c093d4149ced2ba8abd20923e00
sstepss <80715867+sstepss@users.noreply.github.com>
2021-04-06 10:33:33 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -20,7 +20,26 @@ create table timings(
 );
 
 insert into users (name, last_name, email)
-    VALUES ('Stepan', 'Borisov', 's_steps@mail.ru'),
-           ('Mike', 'Tyson', 'mike.t2000@mail.ru');
+VALUES ('Stepan', 'Borisov', 's_steps@mail.ru'),
+       ('Mike', 'Tyson', 'mike.t2000@mail.ru');
 
 select id, name, last_name, email from users;
+
+alter table timings
+add user_id int;
+
+insert into users (name, last_name, email)
+VALUES ('bob', 'Borisov', 'bob@mail.ru'),
+       ('Harry', 'Stone', 'sss@mail.ru'),
+       ('Masha', 'Philipova', 'mash@mail.ru');
+
+select id, name, last_name, email from users where name = 'bob';
+
+select id, name, last_name, email from users where id = 2;
+
+select id, name, last_name, email from users where last_name = 'Borisov' and name = 'Stepan';
+
+update users
+set name = 'Robot' where name = 'bob';
+
+delete from users where id = 4;
